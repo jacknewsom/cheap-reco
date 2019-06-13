e228ea110bba7092fa4a31ba0e52f2e0f0092184
Jack Newsom <jackmnewsom@gmail.com>
2019-06-13 11:08:34 -0700

    removed pileup.py

--- pileup.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def shift_and_sum_events(events):
-    shifted_events = []
-    for event in events:
-        shift = np.random.random_integers(-95, 95)
-        shifted_event = event + np.full(event.shape, shift)
-        shifted_events.append(shifted_event)
-    return shifted_events
