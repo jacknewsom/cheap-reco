6c7f585d5f3b6f545b0e745d530cf5037c34fcd6
codyswain <cwswain@cori03.nersc.gov>
2019-08-13 10:25:31 -0700

    Fixed function descriptions

--- pca.py
+++ pca.py
@@ -3,39 +3,50 @@ import numpy as np
 import math
 
 def t(p, q, r):
-    ''' Helper function for vertex association
+    ''' Helper function for closest_vertex
     Purpose: Calculate t param (describes closest point on a line to vertex r)
-    p : cord1 of pc
-    q : cord2 of pc
-    r : vertex '''
+
+    --- Parameters ---
+    p - cord1 of pc [1x3]
+    q - cord2 of pc [1x3]
+    r - vertex [1x3] '''
     x = p-q
     return np.dot(r-q, x)/np.dot(x, x)
 
 def dist(p, q, r):
-    ''' Helper function for vertex association
-    Purpose: Calculate distance from vertex r to the line traced from p to q'''
+    ''' Helper function for closest_vertex
+    Purpose: Calculate distance from vertex r to the line traced from p to q
+
+    --- Parameters ---
+    p : cord1 of pc [1x3]
+    q : cord2 of pc [1x3]
+    r : vertex [1x3]
+    '''
     return np.linalg.norm(t(p, q, r)*(p-q)+q-r)
 
-def associate_vertex(p, q, V):
-    ''' Finds the closest vertex to given pc
-    p -- cord1 of pc
-    q -- cord2 of pc
-    V -- List of all vertices '''
+def closest_vertex(p, q, V):
+    ''' Helper function for pca_vertex_association
+    Purpose: Finds the closest vertex to given pc
+    
+    --- Parameters ---
+    p : cord1 of pc [1x3]
+    q : cord2 of pc [1x3]
+    V : List of all vertices '''
     distances = [dist(p, q, r) for r in V]
-    min_dist = min(distances)
-    vertex_idx = distances.index(min_dist)    
+    min_distance = min(distances)
+    vertex_idx = distances.index(min_distance)    
     return V[vertex_idx]
 
 def pca(H, V):
     ''' Principal Component Analysis
     Purpose: Find Principal Component of a cluster of hit points
 
-    Input
-    H : hit points matrix
+    --- Parameters ---
+    H : Hit points cluster [Nx3]
     V : List of all vertices 
 
-    Output
-    Cordinates of associated vertex'''
+    --- Output ---
+    Points p and q lying on principal component (each 1x3) '''
     
     pca = PCA(n_components=3)
     pca.fit(H)
@@ -43,11 +54,25 @@ def pca(H, V):
     pc_y = pca.components_[:, 1]
     pc_z = pca.components_[:, 2]
     pc_mag = pca.explained_variance_[0]
-                             
+    
     p = np.array([pca.mean_[0], pca.mean_[1], pca.mean_[2]])
     q = np.array([pca.mean_[0] + pc_x[0] * math.sqrt(pc_mag),
                   pca.mean_[1] + pc_y[0] * math.sqrt(pc_mag),
                   pca.mean_[2] + pc_z[0] * math.sqrt(pc_mag)])
+    return p, q
+    
+    
+def pca_vertex_association(H, V):
+    ''' Main function
+    Purpose: Associate a cluster with a vertex using pca
+    
+    --- Parameters ---
+    H : Hit points cluster [Nx3]
+    V : List of vertices
+    
+    --- Output ---
+    Vertex (1x3) from list V, associated with cluster H'''
 
-    # Calculate which vertex is closest
-    return associate_vertex(p, q, V)
+    p, q = pca(H)
+    return closest_vertex(p, q, V)
+    
