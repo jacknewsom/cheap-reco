Fixed voxelization binning and energywise accuracy calculation

ArCubeROOT_to_HDF5.py
@@ -46,7 +46,6 @@ def convert_event_data(event):
     x, y, z = list(event.xq), list(event.yq), list(event.zq)
     pid, energies = list(event.pidq), list(event.dq)
     labels = [pdg_to_particlebomb_coding(p) for p in pid]
-    
     return x, y, z, energies, labels
 
 def get_entries_per_event(argon, start, end):
@@ -77,7 +76,6 @@ def raw_arcube_file_loader(argon, batch_size):
             features[c_ind: end] = np.array(energies).reshape((len(energies), 1))
             labels[c_ind: end] = np.array(_labels).reshape((len(_labels), 1))
             c_ind += len(x)
-
         yield -1, coordinates, features, labels
 
 def inside_region(voxel, dimension):
@@ -89,29 +87,35 @@ def inside_region(voxel, dimension):
             return False
     return True
 
-def voxelize(coordinates, features, labels, dimension=192):
+def voxelize(coordinates, features, labels):
     '''Convert discrete data from ArCube into voxelized data
     with resolution 0.3cm (so that a voxel 0.3cm on side)
+    
+    All energies in a given voxel will be added together.
+
+    The label for a given voxel is ill-defined, since
+    many particles with different labels may pass
+    through a given voxel. For now, voxels are simply
+    given at random the label of one of the particles 
+    passing through their volume.
     '''
-    uncentered_voxels = (coordinates / 0.3).astype('int')
-    rows_in_region, features_in_region, labels_in_region = [], [], []
-    for row in range(uncentered_voxels.shape[0]):
-        if inside_region(uncentered_voxels[row], dimension):
-            rows_in_region.append(uncentered_voxels[row])
-            features_in_region.append(features[row])
-            labels_in_region.append(labels[row])
-    legal_voxels = np.array(rows_in_region)
-    legal_features = np.array(features_in_region)
-    legal_labels = np.array(labels_in_region)
-    return legal_voxels + np.full(legal_voxels.shape, (dimension-1)/2), legal_features, legal_labels
+    voxels = (coordinates / 0.3).astype('int')
+    unique_voxels = {}
+    for i in range(voxels.shape[0]):
+        index = tuple(voxels[i])
+        if index not in unique_voxels:
+                 unique_voxels[index] = {"feature": 0, "label": 0} 
+        unique_voxels[index]["feature"] += features[i]
+        unique_voxels[index]["label"] = labels[i]
+    unique_coordinates = np.vstack(unique_voxels.keys())
+    unique_features = np.vstack([unique_voxels[i]["feature"] for i in unique_voxels])
+    unique_labels = np.vstack([unique_voxels[i]["label"] for i in unique_voxels])
+    return unique_coordinates, unique_features, unique_labels
     
 def voxelized_arcube_file_loader(argon, batch_size, dimension):
     for _, c, f, l in raw_arcube_file_loader(argon, batch_size):
-        # voxels, features, labels = voxelize(c, f, l, dimension)
-        voxels = (c / 0.3).astype('int')
-        if c.shape[0] > 0 and voxels.shape[0] == 0:
-            continue
-        yield dimension, voxels, f, l
+        voxels, features, labels = voxelize(c, f, l)
+        yield dimension, voxels, features, labels
 
 def ArCube_to_HDF5(filename, dimension, noisy=False):
     arcube = TFile(filename)

clustering.py
@@ -10,12 +10,12 @@ def dbscan(data, epsilon, min_samples):
     '''
     return DBSCAN(eps=epsilon, min_samples=min_samples, metric='euclidean').fit(data).labels_
 
-def find_signal(cluster_labels):
+def find_signal_and_noise(cluster_labels):
     ''' points labeled noise (i.e. points with labels -1)
     '''
-    return np.where(cluster_labels != -1)
+    return np.where(cluster_labels != -1), np.where(cluster_labels == -1)
 
-def find_big_clusters(cluster_labels, cut_size):
+def find_big_and_small_clusters(cluster_labels, cut_size):
     '''Returns indices of voxels in clusters that are larger
     than cut_size.
 
@@ -26,7 +26,8 @@ def find_big_clusters(cluster_labels, cut_size):
     '''
     unique, sizes = np.unique(cluster_labels, return_counts=True)
     big_clusters = np.isin(cluster_labels, unique[sizes > cut_size])
-    return big_clusters
+    small_clusters = np.isin(cluster_labels, unique[sizes <= cut_size])
+    return big_clusters, small_clusters
 
 def cluster_and_cut(data, labels, features, cut_size):
     '''Clusters data with DBSCAN, then cuts noise
@@ -42,13 +43,25 @@ def cluster_and_cut(data, labels, features, cut_size):
     # Calculate DBSCAN labels
     cluster_predictions = dbscan(data, best_epsilon, best_min_samples)
 
-    # remove noise points
-    signal_clusters = find_signal(cluster_predictions)
+    # separate signal and noise points
+    signal_clusters, noise_clusters = find_signal_and_noise(cluster_predictions)
     denoised_data, denoised_features = data[signal_clusters], features[signal_clusters]
     denoised_predictions, denoised_labels = cluster_predictions[signal_clusters], labels[signal_clusters]
+    
+    noise_data, noise_features = data[noise_clusters], features[noise_clusters]
+    noise_predictions, noise_labels = cluster_predictions[noise_clusters], labels[noise_clusters]
+    
+    # separate large and small clusters
+    big_clusters, small_clusters = find_big_and_small_clusters(denoised_predictions, cut_size)
+    big_data, big_features = denoised_data[big_clusters], denoised_features[big_clusters]
+    big_predictions, big_labels = denoised_predictions[big_clusters], denoised_labels[big_clusters]
 
-    # remove small clusters
-    big_clusters = find_big_clusters(denoised_predictions, cut_size)
-    cut_data, cut_features = denoised_data[big_clusters], denoised_features[big_clusters]
-    cut_predictions, cut_labels = denoised_predictions[big_clusters], denoised_labels[big_clusters]
-    return cut_data, cut_labels.reshape(-1), cut_features.reshape(-1), cut_predictions
+    small_data, small_features = denoised_data[small_clusters], denoised_features[small_clusters]
+    small_predictions, small_labels = denoised_predictions[small_clusters], denoised_labels[small_clusters]
+
+    all_data = [big_data, small_data, noise_data]
+    all_labels = [big_labels.reshape(-1), small_labels.reshape(-1), noise_labels.reshape(-1)]
+    all_features = [big_features.reshape(-1), small_features.reshape(-1), noise_features.reshape(-1)]
+    all_predictions = [big_predictions, small_predictions, noise_predictions]
+    
+    return all_data, all_labels, all_features, all_predictions

event_generator.py
@@ -29,7 +29,7 @@ def simulate_interaction(event_file):
     # load n_events
     start_index = np.random.randint(0, 999-n_events)
     d, c, f, l, v = load_and_convert_HDF5_to_sparse_np(event_file, n_events, start_index)
-
+    
     # distribute events across larger volume with
     # a uniform random distribution
     x_positions = np.random.randint(0, 1333, n_events)
@@ -60,4 +60,5 @@ def simulate_interaction(event_file):
         legal_vertices.append(vertex)
         legal_features.append(new_feature)
         legal_labels.append(new_label)
+    
     return legal_coordinates, legal_features, legal_labels, legal_vertices

metrics.py
@@ -59,13 +59,26 @@ def energy_purity(energy, predictions, labels):
         purities[label].append(correct_energy / predicted_energy)
     return purities
 
+def energy_accuracy(energy, predictions, labels):
+    '''Calculates energywise clustering accuracy given predictions and labels.
+
+    Keyword arguments:
+    energies -- (N,) numpy array of energies
+    predictions -- (N,) numpy array of predictions
+    labels -- (N,) numpy array of labels. Note that this function
+              assumes that the prediction and label for a given voxel
+              lie at the same position in these arrays.
+    '''
+    correct = np.where(predictions == labels)
+    return sum(energy[correct]) / len(correct)
+
 def number_efficiency(predictions, labels):
     pass
 
 def number_purity(predictions, labels):
     pass
 
-def association_accuracy(predictions, labels):
+def number_accuracy(predictions, labels):
     '''Calculates vertex association accuracy given predictions and labels.
 
     Keyword arguments:
@@ -73,5 +86,5 @@ def association_accuracy(predictions, labels):
     labels -- (N,) numpy array of labels
     '''
     if len(predictions) != len(labels):
-        raise ValueError('length of predictions ({0}) not equal to length of labels ({1])'.format(len(predictions), len(labels))
+        raise ValueError('length of predictions ({0}) not equal to length of labels ({1])'.format(len(predictions), len(labels)))
     return np.sum(predictions == labels) / len(predictions)

runner.py
@@ -2,35 +2,102 @@ import numpy as np
 from event_generator import simulate_interaction
 from clustering import cluster_and_cut
 from association import is_cluster_touching_vertex
+from pca import pca_vertex_association
+from drawing import draw_events_and_vertices as draw
+from metrics import energy_accuracy, number_accuracy
 
-for i in range(100):
+e_accuracies = []
+n_accuracies = []
+for i in range(20):
+    print("Analyzing event %d..." % i)
     # load data
     coordinates = []
     while len(coordinates) == 0:
         coordinates, features, _, vertices = simulate_interaction("data/ArCube_0000.hdf5")
     labels = [c[:, -1].reshape((-1, 1)) for c in coordinates]
-
+    
     # cluster and cut data
-    coordinates, labels, features, predictions = cluster_and_cut(np.vstack(coordinates)[:, :3], np.vstack(labels), np.vstack(features), 25)
+    coordinates, labels, features, predictions = cluster_and_cut(np.vstack(coordinates)[:, :3], np.vstack(labels), np.vstack(features), 0)
+    # just use large signal clusters
+    coordinates, labels, features, predictions = coordinates[0], labels[0], features[0], predictions[0]
 
+    print("\tData loaded")
     # split remaining data into two categories: vertex-touching and not
     cluster_labels = np.unique(predictions)
-    clusters_touching_vertices = []
-    clusters_not_touching_vertices = []
+    clusters_touching_vertices = {}
+    clusters_not_touching_vertices = {}
     for cluster in cluster_labels:
         cluster_data = coordinates[np.where(predictions == cluster)[0]]
         found_touching_vertex = False
         for j in range(len(vertices)):
             if is_cluster_touching_vertex(cluster_data, vertices[j]):
-                clusters_touching_vertices.append((cluster_data, cluster, j))
+                clusters_touching_vertices[cluster] = {"data": cluster_data, "vertex": j}
                 found_touching_vertex = True
                 break
         if not found_touching_vertex:
-            clusters_not_touching_vertices.append((cluster_data, cluster))
-
+            clusters_not_touching_vertices[cluster] = {"data": cluster_data}
+    print("\tTouching cluster association complete")
+    
     # run PCA on clusters that do not touch vertices directly
+    for cluster in clusters_not_touching_vertices:
+        closest_vertex = pca_vertex_association(clusters_not_touching_vertices[cluster]["data"], vertices)
+        for j in range(len(vertices)):
+            if np.all(closest_vertex == vertices[j]):
+                clusters_not_touching_vertices[cluster]["vertex"] = j
+        if "vertex" not in clusters_not_touching_vertices[cluster]:
+            # something's wrong
+            raise RuntimeError("PCA returned vertex not in `vertices`")
+    print("\tPCA cluster association complete")
+        
+    # restructure data for graphing ease
+    for group in [clusters_touching_vertices, clusters_not_touching_vertices]:
+        for cluster in group:
+            cluster_data = group[cluster]['data']
+            cluster_vtx = group[cluster]['vertex']
+            cluster_label = np.full((cluster_data.shape[0], 1), cluster_vtx)
+            group[cluster]['data'] = np.hstack((cluster_data, cluster_label))
+
+    clusters_touching_vertices = [clusters_touching_vertices[c]['data'] for c in clusters_touching_vertices]
+    clusters_not_touching_vertices = [clusters_not_touching_vertices[c]['data'] for c in clusters_not_touching_vertices]
+    if len(clusters_touching_vertices) == 0 and len(clusters_not_touching_vertices) != 0:
+        all_clusters = np.vstack(clusters_not_touching_vertices)
+    elif len(clusters_touching_vertices) != 0 and len(clusters_not_touching_vertices) == 0:
+        all_clusters = np.vstack(clusters_touching_vertices)
+    elif len(clusters_touching_vertices) == 0 and len(clusters_not_touching_vertices) == 0:
+        # no clusters remaining after cut
+        continue
+    else:
+        all_clusters = np.vstack([np.vstack(clusters_touching_vertices), np.vstack(clusters_not_touching_vertices)])
+
+    # calculate accuracy
+    data_dict = {}
+    coords = np.vstack(coordinates)
+    for j in range(coords.shape[0]):
+        if tuple(coords[j]) not in data_dict:
+            data_dict[tuple(coords[j])] = {}
+        if tuple(all_clusters[j][:-1]) not in data_dict:
+            data_dict[tuple(all_clusters[j][:-1])] = {}
+        # label
+        data_dict[tuple(coords[j])]["label"] = labels[j]
+        data_dict[tuple(coords[j])]["energy"] = features[j]
+        # prediction
+        data_dict[tuple(all_clusters[j][:-1])]["prediction"] = all_clusters[j][-1]
+
+    correct_energies = [data_dict[k]["energy"] for k in data_dict if data_dict[k]["prediction"] == data_dict[k]["label"]]
+    e_accuracy = sum(correct_energies) / sum(features)
+    as_list = [data_dict[k]["prediction"] == data_dict[k]["label"] for k in data_dict]
+    n_accuracy = float(sum(as_list)) / len(as_list)
+    print("\tEnergy Accuracy calculated: %.3f" % e_accuracy)
+    print("\tNumber Accuracy calculated: %.3f" % n_accuracy)
+    n_accuracies.append(n_accuracy)
+    e_accuracies.append(e_accuracy)
     
-    
-            
-    
-    
+    # draw graph!
+    draw(coordinates, labels, labels, vertices, "drawings/%d-true.html" % i)
+    draw(all_clusters[:, :3], all_clusters[:, -1], all_clusters[:, -1], vertices, "drawings/%d-pred-%.3f.html" % (i, e_accuracy))
+    print("\tGraphs saved")
+
+n_accuracy = float(sum(n_accuracies)) / len(n_accuracies)
+e_accuracy = float(sum(e_accuracies)) / len(e_accuracies)
+print("\n\nTotal Energy Accuracy: %.3f" % e_accuracy)
+print("Total Number Accuracy: %.3f" % n_accuracy)