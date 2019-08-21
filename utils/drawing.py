0567c9aa5ca01e2aac07c95219bab7e1ac33447d
Jack Newsom <jackmnewsom@gmail.com>
2019-08-21 14:23:01 -0700

    Restructured drawing functions, changed PCA function to return distance to nearest vertex

--- utils/drawing.py
+++ utils/drawing.py
@@ -2,59 +2,38 @@ import numpy as np
 import plotly.graph_objs as go
 from plotly.offline import plot
 
-def scatter(x, y, z, color, colorscale=None, markersize=2, name=None, hovertext=None, filename='temp.html'):
-    '''Draw 3D scatterplot.
+def scatter_hits(x, y, z, colors, event_text=None):
+    '''Returns plotly Scatter3d object formatted properly
+    for hit data.
 
     Keyword arguments:
     x -- (N,) numpy array of x-coordinates
     y -- (N,) numpy array of y-coordinates
     z -- (N,) numpy array of z-coordinates
-    color -- (N,) array-like object of colors for each of N points
-    colorscale -- maps 'color' argument to actual color of points in scatterplot
-    markersize -- width of points in scatterplot
-    name -- name of dataset
-    hovertext -- (N,) array-like object of labels for each point in scatterplot
-    filename -- name of output file
     '''
-    threed = go.Scatter3d(x=x, y=y, z=z, mode='markers', name=name,
-                      marker = dict(
-                          size=markersize,
-                          color=color,
-                          colorscale=colorscale,
-                          opacity=0.7
-                      ),
-                      hoverinfo=['x', 'y', 'z'] if hovertext is None else ['x', 'y','z', 'text'],
-                      hovertext=hovertext
-                      )
-    return plot([threed], filename=filename)
-
-def draw_events_and_vertices(events, event_colors, event_text, vertices, filename):
-    '''Handles drawing of 3D scatterplot of some number of events and their
-    respective vertices.
-
-    Keyword arguments:
-    events -- (N, 3) numpy array of coordinates
-    event_colors -- (N,) array-like object of colors for each of N events
-    event_text -- (N,) array-like object of labels for each point in events
-    vertices -- (M,) list of (1, 3) numpy arrays
-    filename -- name of output file
-    '''
-    x, y, z = events[:, 0], events[:, 1], events[:, 2]
-    threed = go.Scatter3d(x=x, y=y, z=z, mode='markers', name=None,
+    return go.Scatter3d(x=x, y=y, z=z, mode='markers', name=None,
                           marker=dict(
                               size=2,
-                              color=event_colors,
+                              color=colors,
                               colorscale=None,
                               opacity=0.7
                           ),
                           hoverinfo=['x', 'y', 'z'] if event_text is None else ['x', 'y', 'z', 'text'],
                           hovertext=event_text
                           )
+
+def scatter_vertices(vertices, vtext=None):
+    '''Returns plotly Scatter3d object formatted properly
+    for vertex data.
+    
+    Keyword arguments:
+    vertices -- (M,) list of (1, 3) numpy arrays
+    '''
     vertices = np.vstack(vertices)
     vx, vy, vz = vertices[:, 0], vertices[:, 1], vertices[:, 2]
     vcolors = list(range(vertices.shape[0]))
     vtext = ['Vertex %d' % d for d in range(vertices.shape[0])]
-    vd = go.Scatter3d(x=vx, y=vy, z=vz, mode='markers', name=None,
+    return go.Scatter3d(x=vx, y=vy, z=vz, mode='markers', name=None,
                       marker=dict(
                           size=10,
                           color=vcolors,
@@ -64,4 +43,9 @@ def draw_events_and_vertices(events, event_colors, event_text, vertices, filenam
                       hoverinfo=['x', 'y', 'z'] if vtext is None else ['x', 'y', 'z', 'text'],
                       hovertext=vtext
                       )
-    return plot([threed, vd], filename=filename)
+
+def draw(filename, *scatterplots):
+    '''Take list of events, vertices, and draw both
+    '''
+    scatters = [s for s in scatterplots]
+    return plot(scatters, filename=filename)
